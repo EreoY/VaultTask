0b78d165889066be32947f72a6f3c6b8d1170e62
Pisit Klinnoy <tcmobile0212602@gmail.com>
2026-07-02 17:39:55 +0700

    fix: Remove invisible character from import statement in dashboard_page.dart

--- my_ai_assistant/lib/ui/dashboard/dashboard_page.dart
+++ my_ai_assistant/lib/ui/dashboard/dashboard_page.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+﻿import 'package:flutter/material.dart';
 import '../common/ime_safe_text_field.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
